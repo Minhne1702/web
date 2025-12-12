4fa3cc0323309333ac8d901f45b9a6948f228994
ThanhMinh <thanhminhprovip455@gmail.com>
2025-12-12 07:51:53 +0700

    LAB

---
+++ Lab02/Lab02/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1, int id =0)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = id;
+
+            return View();
+        }
+    }
+}
